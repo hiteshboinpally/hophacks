d93048908a5db453963d3031f1f5722978655b9a
gauravgowda <ggowda@cs.washington.edu>
2020-09-11 23:30:14 -0700

    Started Front-End

--- e-val/src/App.js
+++ e-val/src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
+        <div className="top">
+      <h1>
+        Vehicle Finder
+      </h1>
+        </div>
+        <link href="https://fonts.googleapis.com/css?family=Be+Vietnam|Nunito|Quicksand&display=swap" rel="stylesheet"/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Please choose the make and model type of your car along with your origin and
+            destination point.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
