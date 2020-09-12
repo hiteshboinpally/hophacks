b2b10c00491e611d82c680736ad6eee85a6fb9a6
Aditya Balasubramanian <balasa@cs.washington.edu>
2020-09-11 23:48:22 -0700

    Dropdowns added for vehicle information

--- e-val/src/App.js
+++ e-val/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CarInformation from "./CarInformation";
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    <div className="App">
-        <div className="top">
-      <h1>
-        Vehicle Finder
-      </h1>
+  render() {
+    return (
+
+        <div className="App">
+          <div className="top">
+            <h1>
+              Vehicle Finder
+            </h1>
+          </div>
+          <link href="https://fonts.googleapis.com/css?family=Be+Vietnam|Nunito|Quicksand&display=swap"
+                rel="stylesheet"/>
+          <header className="App-header">
+            <p>
+              Please choose the make and model type of your car along with your origin and
+              destination point.
+            </p>
+          <CarInformation/>
+          </header>
         </div>
-        <link href="https://fonts.googleapis.com/css?family=Be+Vietnam|Nunito|Quicksand&display=swap" rel="stylesheet"/>
-      <header className="App-header">
-        <p>
-          Please choose the make and model type of your car along with your origin and
-            destination point.
-        </p>
-
-      </header>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
+
+
+
+
